6ec25abb834a5760bac02293bb4ccc1fcb1864b9
jtontiwith <jtonti@gmail.com>
2021-09-07 13:12:26 -0500

    initial commit

---
+++ pages/about.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>About page here.</div>
+</template>
+
+<script>
+export default {}
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>yo</div>
+</template>
+
+<script>
+export default {}
+</script>
